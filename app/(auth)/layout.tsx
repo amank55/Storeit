f69e66887881b9bccc2d883c7743b1dbb30ee0ac
amank55 <amankumarr2005ak@gmail.com>
2025-08-08 10:19:35 +0530

    feat: enhance layout and add typography styles for better readability

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,19 @@ import Image from "next/image";
 import React from "react";
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className="min-h-screen">
-        <section>
+    <div className=" flex min-h-screen">
+        <section className="bg-brand p-10">
           <div>
-            <Image src=""}/>
+            <Image src="/favicon.ico" 
+            alt="logo" 
+            width={16}
+            height={16}
+            className="h-auto"/>
+          </div>
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your Files the best way</h1>
+            <p className="body-1">This is a place where you can store all your documents</p>
           </div>
         </section>
         {children}
