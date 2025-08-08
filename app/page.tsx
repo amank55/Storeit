566d7096fc72ec69dc276d85366c4e12ff5f6948
amank55 <amankumarr2005ak@gmail.com>
2025-08-08 09:50:17 +0530

    style: enhance global styles and scrollbar customization; update page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const page = () => {
   return (
-    <div>pagess</div>
+    <div>
+      <h1 className="font-bold text-3xl text-black flex-center h-screen">StoreIt-The only storage solution you need</h1>
+    </div>
   )
 }
 export default page
